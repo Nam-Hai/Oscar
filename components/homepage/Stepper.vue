e8611984dfe99d4bfa5e79c00ac031e535214f92
Nam Hai <lucas.tran@imt-atlantique.net>
2023-11-25 18:54:09 +0100

    .

---
+++ components/homepage/Stepper.vue
@@ -0,0 +1,59 @@
+<template>
+    <div ref="wrapperRef" class="stepper__wrapper">
+        <div class="left d">
+            01
+        </div>
+        <div class="step__wrapper">
+            <div class="step" v-for="i in 4"></div>
+        </div>
+        <div class="right d">
+            04
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+// const {propName = fallbackValue} = defineProps<{propName: type}>()
+// const emits = defineEmits([])
+
+const store = useStore()
+
+const wrapperRef = ref() as Ref<HTMLElement>
+
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/shared.scss" as *;
+
+.stepper__wrapper {
+    position: absolute;
+    bottom: 2.4rem;
+    left: 50%;
+    transform: translateX(-50%);
+
+    display: flex;
+    column-gap: 1.6rem;
+    align-items: baseline;
+
+    .d {
+        font-size: 1.1rem;
+        font-weight: 400;
+        line-height: 0;
+        /* 136.364% */
+    }
+
+    .step__wrapper {
+        display: flex;
+        column-gap: 0.4rem;
+
+        .step {
+            height: 0.8rem;
+            width: 1px;
+            background-color: $white;
+
+            &.active {
+                transform: scaleY(1.2);
+            }
+        }
+    }
+}</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
     <main ref="mainRef">
+        <h1>Viadomo©DECO</h1>
+
+        <div class="flavor">
+            <div class="flavor-main">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, exercitationem
+            </div>
+            <div class="flavor-sub">
+                <div>Type</div>
+                <div>Field</div>
+                <div>Date</div>
+            </div>
+        </div>
+
+        <Stepper />
     </main>
 </template>
 
@@ -28,7 +42,42 @@ usePageFlow({
 @use "@/styles/shared.scss" as *;
 
 main {
+    height: 100vh;
+    width: 100vw;
     top: 0;
     // background-color: black;
+    color: $white;
+}
+
+.flavor {
+    position: absolute;
+    top: calc(50% + 3rem + 2.4rem);
+    left: calc(50% + 2rem);
+    font-size: 1.3rem;
+    line-height: 100%;
+
+    .flavor-main {
+        margin-bottom: 3.6rem;
+        width: 42rem;
+    }
+
+    .flavor-sub {
+        display: flex;
+        flex-direction: column;
+        row-gap: 0.8rem;
+    }
+}
+
+h1 {
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 8.8rem;
+    font-weight: 500;
+    line-height: 0;
+    letter-spacing: -0.088rem;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
