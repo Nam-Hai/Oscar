2d41f248244a18cfb651b6820c24e2ebc05f22f2
Nam Hai <lucas.tran@imt-atlantique.net>
2024-03-08 19:53:04 +0100

    test

--- pages/project-page/[...id].vue
+++ pages/project-page/[...id].vue
@@ -1,13 +1,13 @@
 <template>
   <main class="project__wrapper" ref="wrapperRef">
-    <Teleport to=".app__wrapper" v-if="isMobile != true">
+    <!-- <Teleport to=".app__wrapper" v-if="isMobile != true">
       <div class="scroll-display-f__wrapper" :style="{ clipPath: `inset(0 0 ${clipPercentage * 100}% 0)` }">
         <div class="scroll-display-f" :class="{ dark: pickerDark, show: scrollDisplayShow }"
           :style="{ transform: translate }">
           {{ scrollPercent }}
         </div>
       </div>
-    </Teleport>
+    </Teleport> -->
 
     <Landing :id="id" />
     TEST
